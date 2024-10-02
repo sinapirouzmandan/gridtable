c93cbc034de8ab1a84fbdba61a7acee09fca2ab9
unknown <xina0x@protonmail.com>
2024-10-02 13:48:34 +0330

    finalize grid design

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,6 +2,56 @@ import React, { useState } from "react";
 import { IGridData } from "../../types/gridData";
 
 const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
+  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
+  const [hoveredColumn, setHoveredColumn] = useState<number | null>(null);
+
+  const getBackgroundColor = (
+    value: number | null,
+    rowIndex: number,
+    colIndex: number
+  ): string => {
+    if (value === null) return "";
+    if (
+      hoveredRow !== null &&
+      hoveredColumn === null &&
+      rowIndex !== hoveredRow
+    )
+      return "";
+    if (
+      hoveredColumn !== null &&
+      hoveredRow === null &&
+      colIndex !== hoveredColumn
+    )
+      return "";
+
+    if (value >= 0) {
+      const opacity = (value * 6) / 100;
+      return `rgba(51, 181, 89, ${opacity})`;
+    } else {
+      const opacity = (Math.abs(value) * 10) / 100;
+      return `rgba(239, 95, 95, ${opacity})`;
+    }
+  };
+
+  const toPersianNumeral = (en: number | string): string => {
+    return ("" + en).replace(/[0-9]/g, function (t) {
+      return "٠١٢٣٤٥٦٧٨٩".slice(+t, +t + 1);
+    });
+  };
+
+  const getExtraRowBackground = (index: number, value: any) => {
+    if (index === 0) {
+        if (value >= 0) {
+            const opacity = (value * 6) / 100;
+            return `rgba(51, 181, 89, ${opacity})`;
+          } else {
+            const opacity = (Math.abs(value) * 10) / 100;
+            return `rgba(239, 95, 95, ${opacity})`;
+          }
+    }
+    return "rgba(209, 213, 219, 0.4)"; 
+  };
+
   return (
     <div className="overflow-x-auto rounded-lg text-[16px] pl-8 pr-8" dir="rtl">
       {data && columns.length > 0 && rows.length > 0 ? (
@@ -9,8 +59,8 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
           <div
             className="grid border-t border-r border-l rounded-lg"
             style={{
-                gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))`,
-              }}
+              gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))`,
+            }}
           >
             <div className="px-4 py-2 text-center mb-[16px] pr-4 pl-5 bg-gray-100">
               سال
@@ -19,6 +69,8 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
               <div
                 key={colIndex}
                 className="px-4 py-2 text-center mb-[16px] bg-gray-100"
+                onMouseEnter={() => setHoveredColumn(colIndex)}
+                onMouseLeave={() => setHoveredColumn(null)}
               >
                 {col}
               </div>
@@ -28,28 +80,33 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
               <React.Fragment key={rowIndex}>
                 <div
                   className="px-4 py-4 text-right ml-10 align-middle bg-white"
-    
+                  onMouseEnter={() => setHoveredRow(rowIndex)}
+                  onMouseLeave={() => setHoveredRow(null)}
                 >
-                  {year}
+                  {toPersianNumeral(year)}
                 </div>
                 {data[rowIndex]?.map((value, colIndex) => (
                   <div
                     key={colIndex}
                     className="px-4 py-2 text-center m-2 rounded-md"
+                    style={{
+                      backgroundColor: getBackgroundColor(value, rowIndex, colIndex),
+                    }}
                   >
-                    {value !== null ? value + "%" : "-"}
+                    {value !== null ? toPersianNumeral(value) + "%" : "-"}
                   </div>
                 ))}
               </React.Fragment>
             ))}
           </div>
 
+          <div className="my-4 border-t border-gray-300 opacity-50"></div>
 
           <div
             className="grid border-r border-l border-b rounded-lg"
             style={{
-                gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))`,
-              }}
+              gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))`,
+            }}
           >
             {extras?.map((extra, index) => (
               <React.Fragment key={extra.key}>
@@ -62,9 +119,11 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
                   <div
                     key={colIndex}
                     className="px-4 py-2 text-center m-2 rounded-md"
-       
+                    style={{
+                      backgroundColor: getExtraRowBackground(index, value),
+                    }}
                   >
-                    {value !== null ? value + "%" : "-"}
+                    {value !== null ? toPersianNumeral(value) + "%" : "-"}
                   </div>
                 ))}
               </React.Fragment>
