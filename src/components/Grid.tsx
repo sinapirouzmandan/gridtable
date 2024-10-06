d203df5edb4758d7e6e873b20c169fcc682ff882
unknown <xina0x@protonmail.com>
2024-10-06 10:19:31 +0330

    adjust UI

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -9,36 +9,48 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
       return "";
     
     const opacity = value >= 0 ? (value * 6) / 100 : (Math.abs(value) * 10) / 100;
-    return `rgba(${value >= 0 ? "51, 181, 89" : "239, 95, 95"}, ${opacity})`;
+    return `rgba(${value >= 0 ? "0, 100, 0" : "210,0,0"}, ${opacity})`;
   };
 
-  const toPersianNumeral = (num: number | string): string =>
-    ("" + num).replace(/[0-9]/g, (t) => "٠١٢٣٤٥٦٧٨٩"[+t]);
+  const toPersianNumeral = (num: number): string =>
+  {
+  num = Math.floor(num)
+   return ("" + num).replace(/[0-9]/g, (t) => "٠١٢٣٤٥٦٧٨٩"[+t]);
+
+  }
 
   const getExtraRowBackground = (index: number, value: number) => {
     const opacity = value >= 0 ? (value * 6) / 100 : (Math.abs(value) * 10) / 100;
-    return index === 0 ? `rgba(${value >= 0 ? "51, 181, 89" : "239, 95, 95"}, ${opacity})` : "rgba(209, 213, 219, 0.4)";
+    return index === 0 ? `rgba(${value >= 0 ? "0, 100, 0" : "210,0, 0"}, ${opacity})` : "rgba(209, 213, 219, 0.4)";
   };
 
   return (
     <div className="overflow-x-auto rounded-lg text-[16px] pl-8 pr-8" dir="rtl">
       {data && columns.length && rows.length ? (
         <>
-          <div
-            className="grid border-t border-r border-l rounded-lg"
+         <div
+            className="grid rounded-lg overflow-hidden mb-4"
             style={{ gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))` }}
           >
-            <div className="px-4 py-2 text-center mb-[16px] pr-4 pl-5 bg-gray-100">سال</div>
+           <div className="px-4 py-2 text-center pr-4 pl-8 bg-gray-100">سال</div>
             {columns.map((col, colIndex) => (
               <div
                 key={colIndex}
-                className="px-4 py-2 text-center mb-[16px] bg-gray-100"
+                className="px-4 py-2 text-center bg-gray-100"
                 onMouseEnter={() => setHovered({ ...hovered, col: colIndex })}
                 onMouseLeave={() => setHovered({ ...hovered, col: null })}
               >
                 {col}
               </div>
+              
             ))}
+            </div>
+            <div className="border-[1.6px] rounded-lg overflow-hidden">
+            <div
+            className="grid pt-4"
+            style={{ gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))` }}
+          >
+        
 
             {rows.map((year, rowIndex) => (
               <React.Fragment key={rowIndex}>
@@ -47,7 +59,7 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
                   onMouseEnter={() => setHovered({ ...hovered, row: rowIndex })}
                   onMouseLeave={() => setHovered({ ...hovered, row: null })}
                 >
-                  {toPersianNumeral(year)}
+                  {toPersianNumeral(Number(year))}
                 </div>
                 {data[rowIndex]?.map((value, colIndex) => (
                   <div
@@ -62,15 +74,15 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
             ))}
           </div>
 
-          <div className="my-4 border-t border-gray-300 opacity-50"></div>
+          <div className="my-4 border-[1.5px] opacity-90 w-[98%] mr-auto ml-auto"></div>
 
           <div
-            className="grid border-r border-l border-b rounded-lg"
+            className="grid pb-4"
             style={{ gridTemplateColumns: `minmax(80px, 1fr) repeat(${columns.length}, minmax(80px, 1fr))` }}
           >
             {extras?.map((extra, index) => (
               <React.Fragment key={extra.key}>
-                <div className="px-4 py-4 text-right ml-10 align-middle bg-white">{extra.title}</div>
+                <div className="text-right align-middle bg-white pr-4 py-4">{extra.title}</div>
                 {extra.values.map((value, colIndex) => (
                   <div
                     key={colIndex}
@@ -83,6 +95,8 @@ const Grid: React.FC<IGridData> = ({ columns, rows, data, extras }) => {
               </React.Fragment>
             ))}
           </div>
+            </div>
+
         </>
       ) : (
         <p className="p-4">Loading...</p>
